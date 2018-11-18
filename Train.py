b4ca8990498306e1c2e6402f120aa0de035b66a5
Prateek <prateek.shroff@tamu.edu>
2018-11-17 18:02:59 -0600

    Training code

--- Train.py
+++ Train.py
@@ -1,17 +1,100 @@
 import torchvision
+import torch
+from torch.autograd import Variable
+import torch.optim as optim
+
+from CrossEntropyLoss import CE_loss
 
 class training:
 
     def __init__(self, args):
-        self.args = args
+        self.model = args.model
+        self.batch_size = args.batch_size
+        self.val_batch_size = args.val_batch_size
+        self.num_iterations = args.num_iterations
+        self.gpu = args.gpu
+        self.pretrained = args.pretrained
+        self.epoch = args.epoch
+        self.save_directory = args.save_directory
+        self.resume = args.resume
+        self.lr = args.lr
+        self.lr_update_iter = args.lr_update_iter
+        self.loss_arr = []
+        #optimizer
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr, weight_decay=1e-3)
+
+    def update_lr(self, lr):
+        for param_group in self.optimizer.param_groups:
+            param_group['lr'] = lr
+
+    def restore(self,resume_iter):
+        path = os.path.join(self.save_directory, './net_%d.pth'%(resume_iter))
+        checkpoint_dict = torch.load(path)
+        self.model.load_state_dict(checkpoint_dict['state_dict'])
+        self.lr = checkpoint_dict['lr']
+        return checkpoint_dict['iteration']
+
 
     def train(self, train_loader, val_loader):
 
         train_iter = iter(train_loader)
-        data, label  = train_iter.next()
 
-        val_iter = iter(val_loader)
-        data2, label  = val_iter.next()
+        start_iter = 0
+        if self.resume:
+            # TODO
+            start_iter = self.restore(self.resume)
+        #transfer to GPU
+        model = self.model.cuda()
 
-        print(data.shape, '    ', data2.shape)
-        torchvision.utils.save_image(data2,'test1.png', nrow=2)
+        #Set to training mode
+        model.train()
+
+        #define loss
+        criteria = CE_loss()
+
+        print('..........................Starting training.................')
+
+        #start training
+        for i, data in train_loader:
+            img, _ = data
+
+            img = Variable(img).cuda()
+
+            output, enc = model(img)
+
+            loss = criteria(output, enc)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            #logging
+            if (i+1) % 500 == 0:
+                print('At iteration %d, loss is %.4f'%(i,loss.data())
+                self.loss_arr.append(loss.data())
+
+            #Update learning rate if required
+            if (i+1) % self.lr_update_iter == 0:
+                self.record_iters = i
+                if self.lr > 1e-8:
+                    self.lr *= 0.316
+                self.update_lr(self.lr)
+
+            #validate/Test
+
+
+
+            #checkpoint
+            if (i+1)%2000 == 0:
+                state_dict = model.state_dict()
+                checkpoint = {iteration: i,
+                              state_dict: state_dict,
+                              lr : self.lr}
+                save_path = os.path.join(self.save_directory, './net_%d.pth'%(i+1))
+                torch.save(checkpoint, save_path)
+
+        print('...............Training Completed...........')
+
+
+
+
--- main.py
+++ main.py
@@ -17,11 +17,8 @@ def main(args):
     ])
 
     #Loading the Training Set
-    train_dataset = datasets.ImageFolder(root=args.trainset_path,
-                                               transform=data_transform)
-    train_loader = torch.utils.data.DataLoader(train_dataset,
-                                                 batch_size=args.batch_size, shuffle=True,
-                                                 num_workers=args.num_workers)
+    train_dataset = datasets.ImageFolder(root=args.trainset_path, transform=data_transform)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, pin_memory=True, num_workers=args.num_workers)
 
     #Loading the Validation Set
     val_root_path = os.path.join(args.trainset_path,'ILSVRC2012_img_train')
@@ -29,7 +26,7 @@ def main(args):
     val_dataset = datasets.ImageFolder(root=val_root_path,
                                                transform=data_transform)
     val_loader = torch.utils.data.DataLoader(val_dataset,
-                                                 batch_size=args.batch_size, shuffle=False,
+                                                 batch_size=args.batch_size, shuffle=False, pin_memory=True,
                                                  num_workers=args.num_workers)
     print('Total images in training set: ', len(train_dataset))
     print('Total images in validation set: ', len(val_dataset))
@@ -54,6 +51,8 @@ if __name__=='__main__':
     parser.add_argument('--epoch', type=int, default=1, help='Number of training epoch')
     parser.add_argument('--save_directory', type=str, default='./output', help='Set output directory')
     parser.add_argument('--resume', type=int, default=0, help='Specify the epoch')
+    parser.add_argument('--lr', type=float, default=3.16e-4, help='learning_rate')
+    parser.add_argument('--lr_update_iter', type=int, default=30000, help='Update lr every this iterations')
 
     args = parser.parse_args()
     main(args)
